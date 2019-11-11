c68fc8f2df9fe3a2475ec1165f4f0f0c1d660955
marcospereiro <30293789+NemoIT@users.noreply.github.com>
2019-11-11 17:42:25 +0100

    Add files via upload

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+
+INCIBE_BASE_URL = 'https://www.incibe.es'
+INCIBE_REL_HOME_URL = '/protege-tu-empresa/avisos-seguridad'
+SCRAPING_DELAY_SHORT = 1  # in seconds
+SCRAPING_DELAY_LONG = 5  # in seconds
+OUTPUT_FILENAME = 'INCIBEsecwarn.json'
