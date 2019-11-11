24e938dd94f0ba6816b1a7c7f62edb30190978cd
marcospereiro <30293789+NemoIT@users.noreply.github.com>
2019-11-11 17:46:59 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-sdfs
+from incibe import INCIBEScraper
+import constants
+
+# Crea un objeto 
+scraper = INCIBEScraper();
+
+# Llama a la función principal
+scraper.scrape();
+
+# Guarda los resultados
+scraper.save2json(constants.OUTPUT_FILENAME);
